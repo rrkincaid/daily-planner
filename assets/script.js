6b6b7acc9bfb64b51798eb6a4c196795f5efa094
rrkincaid <rrkincaid@gmail.com>
2022-05-15 16:06:57 -0600

    updates

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var timeBlock = $("#time-block");
+
+var saveButton = $(".save-button");
+
+var inputText = $("#input-text");
+
+//event handler for save button - connects to textinput so that it can be saved to local storage
+
+//event handler for text box so that text dissapears when you start typing
+
