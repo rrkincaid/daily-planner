2d733b17fbb3ca34b25fcb2f8ff7ab0404efd12d
rrkincaid <rrkincaid@gmail.com>
2022-05-15 20:08:33 -0600

    updates

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,29 @@ var timeBlock = $("#time-block");
 
 var saveButton = $(".save-button");
 
-var inputText = $(".input-group");
-
-var lightMode 
-
-var darkMode
-
-// var currentDay = moment();
-// $("#current-day").text(currentDay.format("MMM Do, YYYY"));
+var inputGroup = $(".input-group");
 
 var today = moment();
 $("#current-day").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 
 console.log("today");
 
+
 //event handler for save button - connects to textinput so that it can be saved to local storage
 
-//moment so that jumot recognizes local time
+saveButton.addEventListener("click", function(event) {
+event.preventDefault();
 
-//moment that changes colors in the body of 
+var inputText = {
+  inputGroup: inputGroup.value,
+};
 
-// saveButton.click(event) {
-//     inputText.text()
-//     //input to local storage
-// };
+localStorage.setItem("inputText",
+  JSON.stringify(inputText));
+
+});
+
+//moment that changes colors in the body of text 
+
+
 
