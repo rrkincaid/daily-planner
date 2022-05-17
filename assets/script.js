bdaab4be0c46235d6402fb34fb05f85a5f05ae3f
rrkincaid <rrkincaid@gmail.com>
2022-05-17 09:22:05 -0600

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-var timeBlock = $("#time-block");
+// var timeBlock = $("#time-block");
 
 var saveButton = $(".save-button");
 
-var inputGroup = $(".input-group");
+var todoList = $(".input-group");
 
 var today = moment();
 $("#current-day").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
@@ -10,21 +10,46 @@ $("#current-day").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
 console.log("today");
 
 
-//event handler for save button - connects to textinput so that it can be saved to local storage
 
-saveButton.addEventListener("click", function(event) {
-event.preventDefault();
 
-var inputText = {
-  inputGroup: inputGroup.value,
-};
 
-localStorage.setItem("inputText",
-  JSON.stringify(inputText));
+saveButton.on("click", function (event) {
+var inputValue = ($(this).siblings("input").val())
+  var timeBlock = $(this).attr("class").split(" ")[0]
+  console.log(timeBlock)
 
+  localStorage.setItem(timeBlock, inputValue)
 });
 
-//moment that changes colors in the body of text 
+
+  function todoList() {
+  localStorage.setItem("tasklist", document.getElementById('input-group').value);
+};
 
 
 
+//event handler for save button - connects to textinput so that it can be saved to local storage
+
+// localStorage.setItem("todoList", "inputGroup");
+
+// saveButton.addEventListener("click", function(event) {
+// event.preventDefault();
+
+// var todoList = {
+//   inputGroup: inputGroup.value,
+// };
+
+// localStorage.setItem("todoList",
+//   JSON.stringify(todoList));
+// });
+
+
+
+//moment that changes colors in the body of text
+
+
+// function colorChange(){
+// document.body.children[moment().hour() - 8].children[0].children[1].children[0].setAttribute("style", background: gray")
+//     for (let i = 1, i < moment().hour() - 8; i++)
+//     document.body.children[i].children[0].children[1].children[0].setAttribute("style", background: blue")
+// }
